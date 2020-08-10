cfce3f980c2d555dbc4faa59d10eab8c5d5419ae
Glen Smith <github@smithsrock.com>
2020-08-10 10:03:48 -0600

    Add crypt impl test

--- tests/lib/test_crypt_impl.py
+++ tests/lib/test_crypt_impl.py
@@ -14,6 +14,20 @@ from privacy_playground.lib import crypt_impl
 
 class TestCryptImple(object):
 
-    def test_crypt_impl(self):
-        # create a template
-        pass
+    def setup_method(self):
+        self.pw = "This is just a test"
+        self.salt = "1234abcddeadbeef"
+        self.key = crypt_impl.make_crypt_key(self.pw, self.salt)
+        self.impl = crypt_impl.CryptImpl(
+            self.key, crypt_arg_is_key=True, iv=self.salt
+        )
+
+    def test_encrypt(self):
+        plaintext = "This is just a test"
+        ciphertext = self.impl.encrypt(plaintext)
+        assert ciphertext == '4utME4nulSvCt-65OmM7daAR9w=='
+
+    def test_decrypt(self):
+        ciphertext = '4utME4nulSvCt-65OmM7daAR9w=='
+        plaintext = self.impl.decrypt(ciphertext)
+        assert plaintext == "This is just a test"
