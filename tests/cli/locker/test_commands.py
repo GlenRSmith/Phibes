eba393a3fd4a285d5efd239532b7982544d44e09
Glen Smith <github@smithsrock.com>
2020-08-12 11:09:47 -0600

    cli locker test_create, test_delete (sunny day path)

--- tests/cli/locker/test_commands.py
+++ tests/cli/locker/test_commands.py
@@ -12,7 +12,7 @@ from click.testing import CliRunner
 # Local application/library specific imports
 from phibes.lib.config import Config
 from phibes.lib.locker import Locker
-from phibes.cli.locker.commands import create_locker
+from phibes.cli.locker.commands import create_locker, delete_locker
 
 
 def copy_config(source, target):
@@ -26,17 +26,18 @@ class TestCreateLocker(object):
     name = "new_locker"
     pw = "SmellyBeansVictor"
 
-    @staticmethod
-    def setup_method():
+    def setup_method(self):
         try:
-            Locker.delete(TestCreateLocker.name, TestCreateLocker.pw)
+            Locker.delete(self.name, self.pw)
         except FileNotFoundError:
             pass
         return
 
-    @staticmethod
-    def teardown_method():
-        Locker.delete(TestCreateLocker.name, TestCreateLocker.pw)
+    def teardown_method(self):
+        try:
+            Locker.delete(self.name, self.pw)
+        except FileNotFoundError:
+            pass
         return
 
     def test_create_locker(self, tmp_path, datadir, capsys):
@@ -45,11 +46,41 @@ class TestCreateLocker(object):
             runner = CliRunner()
             result = runner.invoke(
                 create_locker,
-                [
-                    "--locker", TestCreateLocker.name,
-                    "--password", TestCreateLocker.pw
-                ]
+                ["--locker", self.name, "--password", self.pw]
             )
             assert result.exit_code == 0
             assert "created" in result.output
+        return
+
+
+class TestDeleteLocker(object):
+
+    name = "new_locker"
+    pw = "SmellyBeansVictor"
+
+    def setup_method(self):
+        try:
+            Locker.delete(self.name, self.pw)
+        except FileNotFoundError:
+            pass
+        Locker(self.name, self.pw, create=True)
+        return
+
+    def teardown_method(self):
+        try:
+            Locker.delete(self.name, self.pw)
+        except FileNotFoundError:
+            pass
+        return
+
+    def test_delete_locker(self, tmp_path, datadir, capsys):
+        copy_config(datadir["phibes-config.json"], tmp_path)
+        with capsys.disabled():
+            runner = CliRunner()
+            result = runner.invoke(
+                delete_locker,
+                ["--locker", self.name, "--password", self.pw]
+            )
+            assert result.exit_code == 0
+            assert "deleted" in result.output
         return
