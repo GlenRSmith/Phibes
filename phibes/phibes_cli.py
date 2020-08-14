2baa3d5036bc4cdcf71562034fb24418d6bea08f
Glen Smith <github@smithsrock.com>
2020-08-14 12:48:51 -0600

    Reflect decomp in command runtime additions to cli main

--- phibes/phibes_cli.py
+++ phibes/phibes_cli.py
@@ -9,6 +9,9 @@ Click-based command-line interface to phibes
 import click
 
 # in-project modules
+from phibes.cli.item.delete import delete_item as item_delete_cmd
+from phibes.cli.item.edit import edit as item_edit_cmd
+from phibes.cli.item.list import list_items as item_list_cmd
 from phibes.cli.item import commands as item_group
 from phibes.cli.locker import commands as locker_group
 
@@ -28,11 +31,13 @@ def main():
 
 main.add_command(locker_group.create_locker)
 main.add_command(locker_group.delete_locker)
-main.add_command(item_group.edit)
-main.add_command(item_group.list_items)
-main.add_command(item_group.ls)
+main.add_command(item_edit_cmd)
+main.add_command(item_delete_cmd)
+main.add_command(item_list_cmd)
+# main.add_command(item_group.list_items)
+# main.add_command(item_group.ls)
 main.add_command(item_group.show_item)
-main.add_command(item_group.delete_item)
+# main.add_command(item_group.delete_item)
 
 if __name__ == '__main__':
     main()
