b2d83a676cc6d4fc94da4922fca93b1850932412
Glen Smith <github@smithsrock.com>
2020-08-14 13:46:58 -0600

    Update click command additions on phibes_cli

--- phibes/phibes_cli.py
+++ phibes/phibes_cli.py
@@ -12,7 +12,7 @@ import click
 from phibes.cli.item.delete import delete_item as item_delete_cmd
 from phibes.cli.item.edit import edit as item_edit_cmd
 from phibes.cli.item.list import list_items as item_list_cmd
-from phibes.cli.item import commands as item_group
+from phibes.cli.item.get import get_item as item_get_cmd
 from phibes.cli.locker import commands as locker_group
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -32,11 +32,11 @@ def main():
 main.add_command(locker_group.create_locker)
 main.add_command(locker_group.delete_locker)
 main.add_command(item_edit_cmd)
+main.add_command(item_get_cmd)
 main.add_command(item_delete_cmd)
 main.add_command(item_list_cmd)
 # main.add_command(item_group.list_items)
 # main.add_command(item_group.ls)
-main.add_command(item_group.show_item)
 # main.add_command(item_group.delete_item)
 
 if __name__ == '__main__':
