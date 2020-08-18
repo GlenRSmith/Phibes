b47088dd438c907ed80a62a1929c78d16cadff29
Glen Smith <github@smithsrock.com>
2020-08-17 18:21:09 -0600

    Update item list command to get args from context shared from click group

--- phibes/cli/item/list.py
+++ phibes/cli/item/list.py
@@ -14,8 +14,6 @@ from phibes.lib.locker import registered_items
 
 
 @click.command(name='list')
-@click.option('--locker', prompt='Locker')
-@click.option('--password', prompt='Password', hide_input=True)
 @click.option(
     '--item_type',
     prompt='Type of item to list',
@@ -24,15 +22,17 @@ from phibes.lib.locker import registered_items
 )
 @click.option('--verbose', prompt='Verbose', default=False, type=bool)
 @catch_phibes_cli
-def list_items(locker, password, item_type, verbose):
+@click.pass_context
+def list_items(ctx, item_type, verbose):
     """
     Display the (unencrypted) names of all Secrets in the Locker
-    :param locker:
-    :param password:
     :param item_type:
     :param verbose:
     :return:
     """
+    config = ctx.obj['config']
+    locker = ctx.obj['locker']
+    password = ctx.obj['password']
     click.echo(
         present_list_items(locker, password, item_type, verbose)
     )
