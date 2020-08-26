9f555af900c7330fc49ebb054f568b101af481fc
Glen Smith <github@smithsrock.com>
2020-08-26 10:53:18 -0600

    Add errors, update cli errors hierarchy.

---
+++ phibes/lib/errors.py
@@ -0,0 +1,27 @@
+"""
+Support functions for command-line interface modules.
+"""
+
+# core library modules
+
+# third party packages
+
+# in-project modules
+
+
+class PhibesError(Exception):
+
+    def __init__(self, *args):
+        if args:
+            self.message = f"Phibes error: {args[0]}"
+        else:
+            self.message = f"Phibes error: no more info"
+        super().__init__(self.message)
+
+    def __str__(self):
+        msg = getattr(self, "message", "was raised")
+        return f"{self.__class__}: {msg}"
+
+
+class PhibesConfigurationError(PhibesError):
+    pass
