426c6729c164e01f6feba04205848bda373bbc3f
Glen Smith <github@smithsrock.com>
2020-08-27 08:29:24 -0600

    Remove ficture from locker_helper that was already added to contest.py (which is the default that pytest looks for)

--- tests/lib/locker_helper.py
+++ tests/lib/locker_helper.py
@@ -15,23 +15,6 @@ from phibes.lib.item import Item
 from phibes.lib.locker import Locker
 
 
-@pytest.fixture
-def setup_and_teardown(request, tmp_path):
-    """
-    Injecting this fixture into a test method causes that method to
-    use `custom_setup` and `custom_teardown` for setup and teardown.
-
-    This allows the tmp_path fixture to be used in setup and teardown.
-    """
-    if hasattr(request.instance, 'custom_setup'):
-        if callable(getattr(request.instance, 'custom_setup')):
-            request.instance.custom_setup(tmp_path)
-    yield
-    if hasattr(request.instance, 'custom_teardown'):
-        if callable(getattr(request.instance, 'custom_teardown')):
-            request.instance.custom_teardown(tmp_path)
-
-
 class BaseTestClass(object):
 
     test_path = None
