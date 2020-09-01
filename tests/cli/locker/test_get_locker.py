c41e2f0d55131766d21b099f18dce16dabf1581b
Glen Smith <github@smithsrock.com>
2020-08-31 20:31:58 -0600

    Dev remove locker crypt key (#25)
    * Remove Locker property crypt_key which is just wrapped crypt_impl.key
    Anything that really needs it, like tests, can use that access path
    
    * Get rid of the unused salt property on Locker
    
    * Refactoring of locker and cli/locker/test_get_locker
    
    locker:
    - remove plaintext property from Locker. It's not in use, and can be added back if needed.
    - remove Locker._ciphertext, likewise
    - update some exception throws to be Phibes exceptions
    - in Locker.__init__, tighten up handling of locker existing unexpectedly, especially in conflict with what config says about hashing the locker name. The intent is to strongly guard against accidental splitting between two lockers
    - in Locker.__init__, better segregate the code for create and not create
    
    test_get_locker
    - add expectation of PhibesConfigurationError where ... it's expected
    - remove `config_hash_locker_names` from parameterization on TestMatrix, and add a suitable class attribute. It must be known before the test method is called, because a locker is created in custom_setup
    - create separate testing classes for hashed/nothashed locker names for that reason
    - remove `change_hashing_after_create` from parameterization because a different exception is expected when it's true vs. false
    - add a test method for that reason that expects PhibesConfigurationError
    
    * Fix line too long

--- tests/cli/locker/test_get_locker.py
+++ tests/cli/locker/test_get_locker.py
@@ -15,7 +15,8 @@ from phibes.cli.locker.get import get_locker_cmd
 from phibes.cli.lib import PhibesCliNotFoundError
 from phibes.lib.config import ConfigModel
 from phibes.lib.crypto import get_name_hash
-from phibes.lib.errors import PhibesAuthError, PhibesNotFoundError
+from phibes.lib.errors import PhibesAuthError, PhibesConfigurationError
+from phibes.lib.errors import PhibesNotFoundError
 from phibes.lib.locker import Locker, LOCKER_FILE
 from phibes.phibes_cli import main
 
@@ -24,29 +25,22 @@ from tests.cli.click_test_helpers import update_config_option_default
 from tests.lib.locker_helper import ConfigLoadingTestClass
 
 
-params = "command_instance,config_arg,hash_names,change_hashing_after_create"
+params = "command_instance,config_arg"
 command_instances = [get_locker_cmd, main.commands['get-locker']]
 include_config_arg = [False, True]
-config_hash_locker_names = [False, True]
-change_hashing_after_create = [False, True]
 matrix_params = []
-for element in itertools.product(
-    command_instances,
-    include_config_arg,
-    config_hash_locker_names,
-    change_hashing_after_create
-):
+for element in itertools.product(command_instances, include_config_arg):
     matrix_params.append(element)
 
 
-class TestMatrix(ConfigLoadingTestClass):
+class TestMatrixHashed(ConfigLoadingTestClass):
 
     hash_locker_names = True
     name = "test_hashed_new_locker"
     pw = "SmellyBeansVictor"
 
     def custom_setup(self, tmp_path):
-        super(TestMatrix, self).custom_setup(tmp_path)
+        super(TestMatrixHashed, self).custom_setup(tmp_path)
         try:
             Locker.delete(self.name, self.pw)
         except PhibesNotFoundError:
@@ -55,15 +49,17 @@ class TestMatrix(ConfigLoadingTestClass):
         return
 
     def custom_teardown(self, tmp_path):
-        super(TestMatrix, self).custom_teardown(tmp_path)
+        super(TestMatrixHashed, self).custom_teardown(tmp_path)
         try:
             Locker.delete(self.name, self.pw)
         except PhibesNotFoundError:
             pass
+        except PhibesConfigurationError:
+            pass
         return
 
     def prep_and_run(
-            self, config_arg, cmd_inst, hash_names, arg_dict, chg_hash
+            self, config_arg, cmd_inst, arg_dict, chg_hash
     ):
         inst = Locker(self.name, self.pw)
         assert inst
@@ -72,7 +68,7 @@ class TestMatrix(ConfigLoadingTestClass):
         else:
             update_config_option_default(cmd_inst, self.test_path)
             config = ConfigModel()
-            config.hash_locker_names = hash_names
+            config.hash_locker_names = self.hash_locker_names
             self.update_config(config)
             arg_list = []
         arg_list += [
@@ -83,24 +79,14 @@ class TestMatrix(ConfigLoadingTestClass):
             conf = ConfigModel()
             conf.hash_locker_names = not conf.hash_locker_names
             self.update_config(conf)
-        return CliRunner().invoke(cmd_inst, arg_list)
+        ret_val = CliRunner().invoke(cmd_inst, arg_list)
+        return ret_val
 
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.positive
-    def test_found(
-            self,
-            command_instance,
-            config_arg,
-            hash_names,
-            change_hashing_after_create,
-            setup_and_teardown
-    ):
+    def test_found(self, command_instance, config_arg, setup_and_teardown):
         result = self.prep_and_run(
-            config_arg,
-            command_instance,
-            hash_names,
-            {},
-            change_hashing_after_create
+            config_arg, command_instance, {}, chg_hash=False
         )
         assert result
         assert result.exit_code == 0
@@ -118,16 +104,13 @@ class TestMatrix(ConfigLoadingTestClass):
             self,
             command_instance,
             config_arg,
-            hash_names,
-            change_hashing_after_create,
             setup_and_teardown
     ):
         result = self.prep_and_run(
             config_arg,
             command_instance,
-            hash_names,
             {'name': self.name + "mangle"},
-            change_hashing_after_create
+            chg_hash=False
         )
         assert result
         assert result.exit_code == 1
@@ -139,20 +122,12 @@ class TestMatrix(ConfigLoadingTestClass):
 
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.negative
-    def test_auth_fail(
-            self,
-            command_instance,
-            config_arg,
-            hash_names,
-            change_hashing_after_create,
-            setup_and_teardown
-    ):
+    def test_auth_fail(self, command_instance, config_arg, setup_and_teardown):
         result = self.prep_and_run(
             config_arg,
             command_instance,
-            hash_names,
             {'password': self.pw + "mangle"},
-            change_hashing_after_create
+            chg_hash=False
         )
         assert result
         assert result.exit_code == 1
@@ -161,12 +136,51 @@ class TestMatrix(ConfigLoadingTestClass):
         result = self.prep_and_run(
             config_arg,
             command_instance,
-            hash_names,
             {},
-            change_hashing_after_create
+            chg_hash=False
         )
         assert result
         assert result.exit_code == 0
+        # alert if tests are messing up actual user home dir
+        assert not Path.home().joinpath(self.name).exists()
+        assert not Path.home().joinpath(get_name_hash(self.name)).exists()
+        return
+
+    @pytest.mark.parametrize(params, matrix_params)
+    @pytest.mark.negative
+    def test_conf_hash_issue(self, command_instance, config_arg, setup_and_teardown):
+        result = self.prep_and_run(
+            config_arg,
+            command_instance,
+            {'password': self.pw},
+            chg_hash=True
+        )
+        assert result
+        assert result.exit_code == 1
+        assert type(result.exception) is PhibesConfigurationError
+        # alert if tests are messing up actual user home dir
+        assert not Path.home().joinpath(self.name).exists()
+        assert not Path.home().joinpath(get_name_hash(self.name)).exists()
+        return
+
+
+class TestMatrixUnHashed(TestMatrixHashed):
+
+    hash_locker_names = False
+    name = "test_unhashed_new_locker"
+    pw = "SmellyBeansVictor"
+
+    @pytest.mark.parametrize(params, matrix_params)
+    @pytest.mark.positive
+    def test_found(self, command_instance, config_arg, setup_and_teardown):
+        result = self.prep_and_run(
+            config_arg, command_instance, {}, chg_hash=False
+        )
+        assert result
+        assert result.exit_code == 0
+        targ = self.test_path/self.name/LOCKER_FILE
+        inst = Locker(self.name, self.pw)
+        assert inst.lock_file == targ
         # alert if tests are messing up actual user home dir
         assert not Path.home().joinpath(self.name).exists()
         assert not Path.home().joinpath(get_name_hash(self.name)).exists()
