65f20ff5b7365baad3a15041d19a8606f680367c
Glen Smith <github@smithsrock.com>
2020-09-14 10:21:31 -0600

    Collapse CryptFactory into one class, add module-level functions as prefered interface to be used by clients.

--- phibes/crypto/factory.py
+++ phibes/crypto/factory.py
@@ -2,7 +2,6 @@
 A Factory is a source of crypt/hash implementations.
 All available implementations are registered and accessible by name.
 """
-
 # Built-in library packages
 from __future__ import annotations
 # import abc
@@ -22,8 +21,6 @@ class SingletonMeta(type):
     A class that uses this as its metaclass can only have one instance created.
     """
 
-    # TODO: will this consider the same class imported differently to be
-    # the same class? e.g. Locker vs. locker.Locker
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
@@ -34,34 +31,19 @@ class SingletonMeta(type):
         return cls._instances[cls]
 
 
-class ObjectFactory(object, metaclass=SingletonMeta):
+class CryptFactory(object, metaclass=SingletonMeta):
     """
-    This singleton class is used to register any objects.
-    They are called `builders` because they are callable, and their
-    callables are intended to return newly-created objects.
+    Further extending: the callables registered here are supposed
+    to return an object that implements the CryptIfc.
+    That could be a function that returns an instance,
+    the __init__ of a CryptIfc class, or an instance of a 'factory'
+    class that has a __call__ method that returns a CryptIfc object
     """
+    _default = None
 
     def __init__(self):
         self._objects = {}
         return
-
-    def register_object(self, key, object_arg):
-        self._objects[key] = object_arg
-
-    def list_objects(self):
-        """
-        Convenience method to inspect the registered objects
-        @return: registered objects
-        @rtype: list
-        """
-        return list(self._objects.keys())
-
-
-class CallableFactory(ObjectFactory):
-    """
-    First layer of extension, expects the registered items
-    to be __callable__, and calls them when accessed
-    """
 
     def _create(self, key: str, **kwargs: dict):
         """
@@ -81,33 +63,26 @@ class CallableFactory(ObjectFactory):
             raise ValueError(f"No registered item found with {key=}")
         return item(**kwargs)
 
-
-class CryptFactory(CallableFactory):
-    """
-    Further extending: the callables registered here are supposed
-    to return an object that implements the CryptIfc.
-    That could be a function that returns an instance,
-    the __init__ of a CryptIfc class, or an instance of a 'factory'
-    class that has a __call__ method that returns a CryptIfc object
-    """
-    _default = None
-
     def register_builder(self, key, builder):
-        self.register_object(key, builder)
+        self._objects[key] = builder
         if not self._default:
             # TODO make a more intentional way to set default.
             self._default = key
 
     def list_builders(self):
-        return self.list_objects()
+        """
+        Convenience method to inspect the registered builders
+        @return: registered builders
+        @rtype: list
+        """
+        return list(self._objects.keys())
 
     def create(self, password: str, crypt_id: str = None, **kwargs):
         """
         Get a registered crypt while creating a locker
         password, {type-specific args}
-        Pack usage-specific params into a dict so the ObjectFactory
-        can remain agnostic.
-        It also permits the callables to have varied signatures.
+        Pack usage-specific params into a dict;
+        e.g. `key_rounds` is specific to PBKF2 hashing
         @param password: user-provided creation password
         @type password: str
         @param crypt_id: usually omitted key for entry in factory
@@ -117,22 +92,15 @@ class CryptFactory(CallableFactory):
         @return: Crypt
         @rtype: crypt_ifc.CryptIfc
         """
-        print(f"CryptFactory.create {kwargs=}")
-        print(f"CryptFactory.create {self._default=}")
+        # print(f"CryptFactory.create {kwargs=}")
+        # print(f"CryptFactory.create {self._default=}")
         target_id = (self._default, crypt_id)[bool(crypt_id)]
         return self.get(target_id, password, None, None, **kwargs)
-        # builder = self._objects.get(target_id)
-        # if not builder:
-        #     raise ValueError(self._default)
-        # return builder(password, **kwargs)
 
-    # It's ok to have context position params but only the consistent ones
-    # For example, if the hash method becomes Argon2, "rounds" isn't a thing.
     def get(self, crypt_id, password, pw_hash, salt, **kwargs):
         """
         Called to get a registered crypt initialized with the given
-        params
-        (i.e. after reading the lock file).
+        params (i.e. after reading the lock file).
         @param crypt_id: Registered GUID of the crypt needed
         @type crypt_id: str
         @param password: user-provided password
@@ -146,14 +114,13 @@ class CryptFactory(CallableFactory):
         @return: Crypt
         @rtype: crypt_ifc.CryptIfc
         """
-        print(f"CryptFactory.get {kwargs=}")
+        # print(f"CryptFactory.get {kwargs=}")
         builder = self._objects.get(crypt_id)
-        print(f"CryptFactory builder {builder}")
+        # print(f"CryptFactory builder {builder}")
         if not builder:
             raise ValueError(crypt_id)
         # Return the result of `__call__` on the registered item
-        ret_val = builder(password, pw_hash, salt, **kwargs)
-        return ret_val
+        return builder(password, pw_hash, salt, **kwargs)
 
 
 class CryptWrapper(object):
