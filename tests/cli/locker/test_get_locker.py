6373c96c7fdb1307ab0cc5dec94b05daab97718a
Glen Smith <github@smithsrock.com>
2020-09-25 14:37:31 -0600

    Expand CLI test of get_locker to include all registered crypt types

--- tests/cli/locker/test_get_locker.py
+++ tests/cli/locker/test_get_locker.py
@@ -15,13 +15,12 @@ from phibes.cli.locker.get import get_locker_cmd
 from phibes.cli.lib import PhibesCliNotFoundError
 from phibes.lib.config import ConfigModel
 from phibes.lib.errors import PhibesAuthError, PhibesConfigurationError
-from phibes.lib.errors import PhibesNotFoundError
 from phibes.model import Locker, LOCKER_FILE
 from phibes.phibes_cli import main
 
 # Local test imports
 from tests.cli.click_test_helpers import update_config_option_default
-from tests.lib.locker_helper import ConfigLoadingTestClass
+from tests.lib.locker_helper import PopulatedLocker
 
 
 params = "command_instance,config_arg"
@@ -32,47 +31,52 @@ for element in itertools.product(command_instances, include_config_arg):
     matrix_params.append(element)
 
 
-class TestMatrixHashed(ConfigLoadingTestClass):
+class TestAllCryptTypes(PopulatedLocker):
+
+    def custom_setup(self, tmp_path):
+        super(TestAllCryptTypes, self).custom_setup(tmp_path)
+
+    def custom_teardown(self, tmp_path):
+        super(TestAllCryptTypes, self).custom_teardown(tmp_path)
+
+    def test_good(self, tmp_path):
+        for name in self.lockers.keys():
+            inst = Locker.get(name, self.password)
+            assert inst
+
+
+class TestMatrixHashed(PopulatedLocker):
 
     hash_locker_names = True
-    name = "test_hashed_new_locker"
-    pw = "SmellyBeansVictor"
 
     def custom_setup(self, tmp_path):
         super(TestMatrixHashed, self).custom_setup(tmp_path)
-        try:
-            Locker.delete(self.name, self.pw)
-        except PhibesNotFoundError:
-            pass
-        Locker.create(self.name, self.pw)
-        return
 
     def custom_teardown(self, tmp_path):
         super(TestMatrixHashed, self).custom_teardown(tmp_path)
-        try:
-            Locker.delete(self.name, self.pw)
-        except PhibesNotFoundError:
-            pass
-        except PhibesConfigurationError:
-            pass
-        return
 
     def prep_and_run(
-            self, config_arg, cmd_inst, arg_dict, chg_hash
+            self,
+            config_arg,
+            cmd_inst,
+            arg_dict,
+            chg_hash
     ):
-        inst = Locker.get(self.name, self.pw)
-        assert inst
         if config_arg:
             arg_list = ["--config", self.test_path]
         else:
+            # change the configured working path to the test directory
             update_config_option_default(cmd_inst, self.test_path)
+            # get the current config
             config = ConfigModel()
+            # update it with the setting for this test
             config.hash_locker_names = self.hash_locker_names
+            # persist the changed config
             self.update_config(config)
             arg_list = []
         arg_list += [
-            "--locker", arg_dict.get('name', self.name),
-            "--password", arg_dict.get('password', self.pw)
+            "--locker", arg_dict.get('name', self.locker_name),
+            "--password", arg_dict.get('password', self.password)
         ]
         if chg_hash:
             conf = ConfigModel()
@@ -84,20 +88,22 @@ class TestMatrixHashed(ConfigLoadingTestClass):
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.positive
     def test_found(self, command_instance, config_arg, setup_and_teardown):
-        result = self.prep_and_run(
-            config_arg, command_instance, {}, chg_hash=False
-        )
-        assert result
-        print(f"{result}")
-        assert result.exit_code == 0
-        inst = Locker.get(self.name, self.pw)
-        hash_dir = inst.crypt_impl.hash_name(self.name)
-        targ = self.test_path/hash_dir/LOCKER_FILE
-        assert inst.lock_file == targ
-        # alert if tests are messing up actual user home dir
-        assert not Path.home().joinpath(self.name).exists()
-        assert not Path.home().joinpath(hash_dir).exists()
-        return
+        all_lockers = list(self.lockers.keys()) + [self.locker_name]
+        for lck in all_lockers:
+            result = self.prep_and_run(
+                config_arg, command_instance, {'name': lck}, chg_hash=False
+            )
+            assert result
+            print(f"{result}")
+            assert result.exit_code == 0
+            inst = Locker.get(lck, self.password)
+            hash_dir = inst.crypt_impl.hash_name(self.locker_name)
+            targ = self.test_path/hash_dir/LOCKER_FILE
+            assert inst.lock_file == targ
+            # alert if tests are messing up actual user home dir
+            assert not Path.home().joinpath(self.locker_name).exists()
+            assert not Path.home().joinpath(hash_dir).exists()
+            return
 
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.negative
@@ -107,66 +113,72 @@ class TestMatrixHashed(ConfigLoadingTestClass):
             config_arg,
             setup_and_teardown
     ):
-        result = self.prep_and_run(
-            config_arg,
-            command_instance,
-            {'name': self.name + "mangle"},
-            chg_hash=False
-        )
-        assert result
-        assert result.exit_code == 1
-        assert type(result.exception) is PhibesCliNotFoundError
-        # alert if tests are messing up actual user home dir
-        assert not Path.home().joinpath(self.name).exists()
+        all_lockers = list(self.lockers.keys()) + [self.locker_name]
+        for lck in all_lockers:
+            result = self.prep_and_run(
+                config_arg,
+                command_instance,
+                {'name': lck + "mangle"},
+                chg_hash=False
+            )
+            assert result
+            assert result.exit_code == 1
+            assert type(result.exception) is PhibesCliNotFoundError
+            # alert if tests are messing up actual user home dir
+            assert not Path.home().joinpath(self.locker_name).exists()
         return
 
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.negative
     def test_auth_fail(self, command_instance, config_arg, setup_and_teardown):
-        result = self.prep_and_run(
-            config_arg,
-            command_instance,
-            {'password': self.pw + "mangle"},
-            chg_hash=False
-        )
-        assert result
-        assert result.exit_code == 1
-        assert type(result.exception) is PhibesAuthError
-        # prove the locker is there if you use the right auth
-        result = self.prep_and_run(
-            config_arg,
-            command_instance,
-            {},
-            chg_hash=False
-        )
-        assert result
-        assert result.exit_code == 0
-        # alert if tests are messing up actual user home dir
-        assert not Path.home().joinpath(self.name).exists()
+        all_lockers = list(self.lockers.keys()) + [self.locker_name]
+        for lck in all_lockers:
+            result = self.prep_and_run(
+                config_arg,
+                command_instance,
+                {'name': lck, 'password': self.password + "mangle"},
+                chg_hash=False
+            )
+            assert result
+            assert result.exit_code == 1
+            assert type(result.exception) is PhibesAuthError
+            # prove the locker is there if you use the right auth
+            result = self.prep_and_run(
+                config_arg,
+                command_instance,
+                {'name': lck, 'password': self.password},
+                chg_hash=False
+            )
+            assert result
+            assert result.exit_code == 0
+            # alert if tests are messing up actual user home dir
+            assert not Path.home().joinpath(self.locker_name).exists()
         return
 
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.negative
-    def test_conf_hash_issue(self, command_instance, config_arg, setup_and_teardown):
-        result = self.prep_and_run(
-            config_arg,
-            command_instance,
-            {'password': self.pw},
-            chg_hash=True
-        )
-        assert result
-        assert result.exit_code == 1
-        assert type(result.exception) is PhibesConfigurationError
-        # alert if tests are messing up actual user home dir
-        assert not Path.home().joinpath(self.name).exists()
+    def test_conf_hash_issue(
+            self, command_instance, config_arg, setup_and_teardown
+    ):
+        all_lockers = list(self.lockers.keys()) + [self.locker_name]
+        for lck in all_lockers:
+            result = self.prep_and_run(
+                config_arg,
+                command_instance,
+                {'name': lck, 'password': self.password},
+                chg_hash=True
+            )
+            assert result
+            assert result.exit_code == 1
+            assert type(result.exception) is PhibesConfigurationError
+            # alert if tests are messing up actual user home dir
+            assert not Path.home().joinpath(self.locker_name).exists()
         return
 
 
 class TestMatrixUnHashed(TestMatrixHashed):
 
     hash_locker_names = False
-    name = "test_unhashed_new_locker"
-    pw = "SmellyBeansVictor"
 
     @pytest.mark.parametrize(params, matrix_params)
     @pytest.mark.positive
@@ -176,9 +188,9 @@ class TestMatrixUnHashed(TestMatrixHashed):
         )
         assert result
         assert result.exit_code == 0
-        targ = self.test_path/self.name/LOCKER_FILE
-        inst = Locker.get(self.name, self.pw)
+        targ = self.test_path/self.locker_name/LOCKER_FILE
+        inst = Locker.get(self.locker_name, self.password)
         assert inst.lock_file == targ
         # alert if tests are messing up actual user home dir
-        assert not Path.home().joinpath(self.name).exists()
+        assert not Path.home().joinpath(self.locker_name).exists()
         return
