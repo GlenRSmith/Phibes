7f1dd90871aa73267820db21af4244d363654d26
Glen Smith <github@smithsrock.com>
2020-09-29 12:22:40 -0600

    Add actual salt handling to plain crypt, otherwise it returns false positive validations

--- phibes/crypto/crypt_plain_plain.py
+++ phibes/crypto/crypt_plain_plain.py
@@ -12,6 +12,7 @@ from typing import Optional
 
 # In-project modules
 from phibes.crypto.crypt_ifc import CryptIfc, EncryptionIfc, HashIfc
+from phibes.lib.errors import PhibesAuthError
 
 
 class HashPlain(HashIfc):
@@ -40,6 +41,8 @@ class CryptPlain(EncryptionIfc):
     Encryption implementation for no encryption
     """
 
+    salt_length_bytes = 16  # arbitrary choice
+
     @property
     def key(self):
         """
@@ -57,21 +60,34 @@ class CryptPlain(EncryptionIfc):
         """
         self._key = new_key
 
+    @property
+    def salt(self):
+        return self._salt
+
+    @salt.setter
+    def salt(self, new_salt: str):
+        salt = (new_salt, self.create_salt())[new_salt is None]
+        if len(bytes.fromhex(salt)) != self.salt_length_bytes:
+            raise ValueError(
+                f"salt {new_salt} is {len(bytes.fromhex(salt))} bytes long\n"
+                f"{self.salt_length_bytes} bytes required\n"
+            )
+        self._salt = salt
+
     def __init__(self, key: str, salt: Optional[str] = None, **kwargs):
-        super(CryptPlain, self).__init__(
-            key, salt, **kwargs)
+        super(CryptPlain, self).__init__(key, salt, **kwargs)
         self.key = key
         self.salt = salt
         return
 
-    @staticmethod
-    def create_salt():
+    @classmethod
+    def create_salt(cls):
         """
         Convenience method to get salt of the right length.
         @return: new salt
         @rtype: str, with valid hexadecimal
         """
-        return secrets.token_hex(16)
+        return secrets.token_hex(cls.salt_length_bytes)
 
     def encrypt(self, plaintext: str, salt: Optional[str] = None) -> str:
         val = plaintext.replace("\n", chr(31))
@@ -87,6 +103,14 @@ class CryptPlainPlain(CryptIfc):
     Encryption implementation for no encryption
     """
 
+    @property
+    def salt(self):
+        """
+        salt property accessor
+        :return:
+        """
+        return self._crypter.salt
+
     def __init__(
             self,
             password: str,
@@ -97,11 +121,28 @@ class CryptPlainPlain(CryptIfc):
         super(CryptPlainPlain, self).__init__(
             password, pw_hash, salt, **kwargs
         )
+        creating = password and not pw_hash and not salt
+        if not (password and pw_hash and salt) and not creating:
+            pref = f"{self.__class__.__name__}\n"
+            raise ValueError(
+                f"invalid call to __init__\n"
+                f"valid calls are are:\n"
+                f"{pref}(password)\n"
+                f"{pref}(password, pw_hash, salt)\n"
+                f"{password=} {pw_hash=} {salt=} {kwargs=}"
+            )
         self._hasher = HashPlain(**kwargs)
-        self._crypter = CryptPlain('', salt)
-        # self.key = key
-        self.pw_hash = "pw_hash"
-        self.salt = ("salt", salt)[salt is not None]
+        if creating:
+            salt = CryptPlain.create_salt()
+        key = self._hasher.hash_str(password, salt)
+        self._crypter = CryptPlain(key, salt)
+        auth_key = self.encrypt(self.hash_name(password, salt))
+        if not creating:
+            if not auth_key == pw_hash:
+                raise PhibesAuthError(
+                    f"{pw_hash} does not match {auth_key}"
+                )
+        self.pw_hash = auth_key
         return
 
     def _hash_str(self, message, salt, length):
@@ -123,7 +164,6 @@ class CryptPlainPlain(CryptIfc):
         @param salt: Optional salt
         @return: encrypted string
         """
-        salt = ('', salt)[bool(salt)]
         return self._crypter.encrypt(plaintext, salt)
 
     def decrypt(self, ciphertext: str, salt: Optional[str] = None) -> str:
@@ -133,5 +173,4 @@ class CryptPlainPlain(CryptIfc):
         @param salt: salt used in encryption
         @return: decrypted string
         """
-        salt = ('', salt)[bool(salt)]
         return self._crypter.decrypt(ciphertext, salt)
