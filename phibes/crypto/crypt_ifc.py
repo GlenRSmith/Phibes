ca10ff8c016cd94d1bae79955c534b493bad507b
Glen Smith <github@smithsrock.com>
2020-10-07 14:41:48 -0600

    Changes to Ifc classes - Add docstrings to various - Change HashIfc.hash_str signature: remove salt, rounds, length_bytes, add kwargs - Replace CryptIfc abstract __init__ with an implementation, add `crypt_id` as a param - Add abstract method CryptIfc.create_key

--- phibes/crypto/crypt_ifc.py
+++ phibes/crypto/crypt_ifc.py
@@ -8,48 +8,106 @@ from typing import Optional
 # Third party packages
 
 # In-project modules
+from phibes.lib.errors import PhibesAuthError
 
 
 class HashIfc(abc.ABC):
+    """
+    Interface definition for hash classes
+    """
 
     @abc.abstractmethod
     def __init__(self, **kwargs):
+        """
+        Signature definition for constructing a HashIfc
+        In each implementation, kwargs should contain parameters
+        that will be held constant for a specific registered strategy.
+        For example, for PBKDF2, the fact that SHA256 would be a property
+        of a registered strategy, and not something passed (and possibly
+        changed to e.g. SHA128) for each item being hashed.
+        These kwargs and their values become part of the name of the
+        registered strategy.
+        That does not preclude them from being stored with the
+        crypted items.
+        @param kwargs:
+        @type kwargs:
+        """
         pass
 
     @abc.abstractmethod
-    def hash_str(
-            self, plaintext: str, salt: str, rounds: int, length_bytes: int
-    ) -> str:
+    def hash_str(self, plaintext: str, **kwargs) -> str:
+        """
+        Signature definition for performing a hash on a plaintext.
+        In each implementation, kwargs should contain parameters
+        that may (and possibly must) vary between invocations from
+        the same registered strategy.
+        For example, for PBKDF2, the `salt` used when hashing a string
+        should be unique for each string.
+        These kwargs and their values
+        @param plaintext: text to hash
+        @param kwargs: keyword arguments, variable per implementation
+        @return:
+        """
         pass
 
 
 class EncryptionIfc(abc.ABC):
+    """
+    Interface definition for encryption classes
+    """
 
     @classmethod
     @abc.abstractmethod
     def create_salt(cls):
+        """
+        Creates and returns a salt suitable for this implementation
+        """
         pass
 
     @property
     @abc.abstractmethod
     def key(self):
+        """
+        Returns the encryption key property
+        """
         pass
 
     @key.setter
     @abc.abstractmethod
     def key(self, new_key):
+        """
+        Encryption key property mutator
+        """
         pass
 
     @abc.abstractmethod
     def __init__(self, key: str, salt: Optional[str] = None, **kwargs):
+        """
+        Constructor
+        :param key: encryption key
+        :param salt: salt
+        :param kwargs: kwargs specific to each implementation
+        """
         pass
 
     @abc.abstractmethod
     def encrypt(self, plaintext: str, salt: str) -> str:
+        """
+        Encrypts the plaintext with the salt
+        :param plaintext: str to encrypt
+        :param salt: salt to mix in
+        :return: encrypted value
+        """
         pass
 
     @abc.abstractmethod
     def decrypt(self, ciphertext: str, salt: str) -> str:
+        """
+        Decrypt the ciphertext
+        :param ciphertext: encrypted text
+        :param salt: salt used when encrypting
+        :return: Original plaintext
+        """
         pass
 
 
@@ -75,25 +133,62 @@ The top-level encryption/hashing class needs to expose:
 
 
 class CryptIfc(abc.ABC):
+    """
+    Interface definition for cryption classes
+    These encompass hashing and encryption
+    """
 
-    @abc.abstractmethod
+    HashType = None
+    EncryptType = None
+
     def __init__(
             self,
+            crypt_id: str,
             password: str,
             pw_hash: Optional[str] = None,
             salt: Optional[str] = None,
-            **kwargs: dict
+            **kwargs
     ):
         """
         Common constructor for all Crypt classes
-        @param password: User password, will always be passed
-        @type password: str
-        @param pw_hash: Previously-stored hash, passed for existing items
-        @type pw_hash: str
-        @param salt: Previously-stored salt, passed for existing items
-        @type salt: str
-        @param kwargs: additional parameters required by certain Crypt classes
-        @type kwargs: dict
+        A CryptIfc instance is created/gotten when a locker is created/gotten.
+        Like the Locker, it is created using just the password,
+        and gotten using the password plus
+        the pw_hash and salt from the locker file.
+        :param crypt_id: unique registered ID for this crypt
+        :param password: User password
+        :param pw_hash: Previously-stored hash, passed for existing items
+        :param salt: Previously-stored salt, passed for existing items
+        :param kwargs: additional parameters required by certain Crypt classes
+        """
+        if bool(pw_hash) ^ bool(salt):
+            pref = f"{self.__class__.__name__}\n"
+            raise ValueError(
+                f"invalid call to __init__\n"
+                f"valid calls are are:\n"
+                f"{pref}(password)\n"
+                f"{pref}(password, pw_hash, salt)\n"
+                f"{password=} {pw_hash=} {salt=} {kwargs=}"
+            )
+        self.crypt_id = crypt_id
+        self._hasher = self.HashType(**kwargs)
+        salt = (self.EncryptType.create_salt(), salt)[bool(salt)]
+        key = self.create_key(password, salt)
+        self._encrypt = self.EncryptType(key, salt)
+        self.pw_hash = self.encrypt(self.hash_name(password, salt))
+        if pw_hash and not (self.pw_hash == pw_hash):
+            raise PhibesAuthError(
+                f"{pw_hash} does not match {self.pw_hash}"
+            )
+        return
+
+    @abc.abstractmethod
+    def create_key(self, password: str, salt: str):
+        """
+        Create an encryption key suitable for this implementation
+        :param password: User password to start
+        :param salt: Salt to mix in
+        :return: Encryption key
         """
         pass
 
@@ -148,6 +243,21 @@ def class_has_callable(
         method,
         abstract: Optional[bool] = None
 ):
+    """
+    Helper function to see if a class has a specified method
+    If `abstract` is True, this function will require `method` to be abstract.
+    If `abstract` is False, this function will forbid `method` to be abstract.
+    If `abstract` is None, the function doesn't care whether `method` is
+    abstract.
+    @param cls: The class to test
+    @type cls: type
+    @param method: the name of the method to test
+    @type method: str
+    @param abstract: whether to require/forbid/ignore the method to be abstract
+    @type abstract: bool
+    @return: bool
+    @rtype:
+    """
     is_abs = "__isabstractmethod__"
     return (
         hasattr(cls, method)
