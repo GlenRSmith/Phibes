be9f3cd22a7b482f2be00d1cc59e44942e13bec1
Glen Smith <github@smithsrock.com>
2021-02-15 10:54:45 -0700

    Set up delete locker CLI with classes for anonymous and named lockers, parent class handles requests for both

--- phibes/cli/locker/delete.py
+++ phibes/cli/locker/delete.py
@@ -1,7 +1,6 @@
 """
 Click interface to delete Locker
 """
-
 # core library modules
 # third party packages
 import click
@@ -9,17 +8,15 @@ import click
 # in-project modules
 from phibes.cli.command_base import PhibesCommandBareBase
 from phibes.cli.command_base import config_option
-from phibes.cli.errors import PhibesCliError
+from phibes.cli.lib import get_locker_args
 from phibes.cli.options import locker_name_option
 from phibes.cli.options import locker_path_option
 from phibes.cli.options import password_option
-from phibes.lib.config import ConfigModel, load_config_file
-from phibes.model import Locker
 
 
 class DeleteLockerCmd(PhibesCommandBareBase):
     """
-    Class to create and run click command to create a locker
+    Class to create and run click command to delete a locker
     """
 
     options = {
@@ -34,31 +31,13 @@ class DeleteLockerCmd(PhibesCommandBareBase):
     @staticmethod
     def handle(*args, **kwargs):
         """Delete a Locker"""
-        # parent class sets up environment variables from args
         super(DeleteLockerCmd, DeleteLockerCmd).handle(*args, **kwargs)
-        try:
-            password = kwargs.pop('password')
-        except KeyError as err:
-            raise PhibesCliError(f'missing required param {err}')
-        if 'path' in kwargs:
-            storage_path = kwargs.pop('path')
-        elif 'config' in kwargs:
-            config = kwargs.pop('config')
-            load_config_file(config)
-            storage_path = ConfigModel().store_path
-        else:
-            raise PhibesCliError(
-                f'path param must be provided by param or in config file'
-            )
-        # locker name is optional, only settable from command-line
-        locker = kwargs.pop('locker', None)
-        inst = Locker.get(
-            password=password, name=locker, path=storage_path
-        )
+        locker = kwargs.get('locker', None)
+        inst = get_locker_args(*args, **kwargs)
         click.confirm(
             (
                 f"Will attempt to delete locker with\n"
-                f"{storage_path.resolve()}\n"
+                f"{inst.path.resolve()}\n"
                 f"Enter `y` to accept, `N` to abort"
             ), abort=True
         )
@@ -66,7 +45,7 @@ class DeleteLockerCmd(PhibesCommandBareBase):
             click.echo(f"confirmed lock file {inst.lock_file} exists")
         if inst.path.exists():
             click.echo(f"confirmed locker dir {inst.path} exists")
-        Locker.delete(password=password, name=locker, path=storage_path)
+        inst.delete_instance(name=locker)
         if not inst.lock_file.exists():
             click.echo(f"confirmed lock file {inst.lock_file} removed")
         else:
@@ -75,7 +54,7 @@ class DeleteLockerCmd(PhibesCommandBareBase):
             click.echo(f"confirmed locker dir {inst.path} removed")
         else:
             click.echo(f"locker dir {inst.path} not removed")
-        click.echo(f"Locker deleted from {storage_path}")
+        click.echo(f"Locker deleted from {inst.path}")
 
 
 class DeleteNamedLockerCmd(DeleteLockerCmd):
