13d1046551f07d36690750a4ebe524971f5ae697
Glen Smith <github@smithsrock.com>
2021-02-15 11:02:48 -0700

    Start work on converging item commands to be anonymous/named locker flexible, as Locker CRUD commands have been made to be

--- phibes/cli/item/create.py
+++ phibes/cli/item/create.py
@@ -3,13 +3,64 @@ Click command for `create-item`
 """
 
 # core library modules
-
 # third party packages
+import click
 
 # in-project modules
-from phibes.cli.command_base import ConfigFileLoadingCmd
-from phibes.cli.lib import create_item
+from phibes.cli.command_base import ConfigFileLoadingCmd, PhibesCommandBareBase
+from phibes.cli.command_base import config_option
+from phibes.cli.errors import PhibesCliError
+from phibes.cli.lib import create_item, get_locker_args, create_item_new
 from phibes.cli.options import item_name_option, template_name_option
+from phibes.cli.options import locker_name_option, locker_path_option
+from phibes.cli.options import password_option
+
+
+class NewCreateItemCmd(PhibesCommandBareBase):
+    """
+    Class to create and run click command to create and add an item to a locker
+    """
+
+    options = {
+        'config': config_option,
+        'path': locker_path_option,
+        'password': password_option,
+        'item': item_name_option,
+        'template': template_name_option
+    }
+
+    def __init__(self):
+        super(NewCreateItemCmd, self).__init__()
+
+    @staticmethod
+    def handle(*args, **kwargs) -> None:
+        """Create and add an item to a locker"""
+        super(NewCreateItemCmd, NewCreateItemCmd).handle(*args, **kwargs)
+        template = kwargs.get('template', None)
+        if template == 'Empty':
+            template = None
+        try:
+            item_name = kwargs.pop('item')
+        except KeyError as err:
+            raise PhibesCliError(f'missing required param {err}')
+        inst = get_locker_args(*args, **kwargs)
+        return create_item_new(
+            locker=inst, item_name=item_name, template_name=template
+        )
+
+
+class CreateItemNamedLockerCmd(NewCreateItemCmd):
+
+    options = {
+        'config': config_option,
+        'locker': locker_name_option,
+        'password': password_option,
+        'item': item_name_option,
+        'template': template_name_option
+    }
+
+    def __init__(self):
+        super(CreateItemNamedLockerCmd, self).__init__()
 
 
 class CreateItemCmd(ConfigFileLoadingCmd):
@@ -53,5 +104,5 @@ options = {
     'template': template_name_option
 }
 
-
-create_item_cmd = CreateItemCmd.make_click_command('create-item', options)
+create_item_cmd = CreateItemNamedLockerCmd.make_click_command('create-item')
+# create_item_cmd = CreateItemCmd.make_click_command('create-item', options)
