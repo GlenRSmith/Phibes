060daeb9694850595f2e6d032132918ec4cb1b7b
Glen Smith <github@smithsrock.com>
2021-02-15 10:52:39 -0700

    Create distinct classes to support CLI getting anonymous and named Lockers

--- phibes/cli/locker/get.py
+++ phibes/cli/locker/get.py
@@ -1,45 +1,59 @@
 """
 Click interface to get Locker
 """
-
 # core library modules
-
 # third party packages
 import click
 
 # in-project modules
-from phibes.cli.command_base import ConfigFileLoadingCmd
-from phibes.cli.lib import get_locker
+from phibes.cli.command_base import PhibesCommandBareBase
+from phibes.cli.command_base import config_option
+from phibes.cli.lib import get_locker_args
+from phibes.cli.options import locker_name_option
+from phibes.cli.options import locker_path_option
+from phibes.cli.options import password_option
 
 
-class GetLockerCmd(ConfigFileLoadingCmd):
+class GetLockerCmd(PhibesCommandBareBase):
+    """
+    Class to create and run click command to get a locker
+    """
+
+    options = {
+        'config': config_option,
+        'path': locker_path_option,
+        'password': password_option
+    }
 
     def __init__(self):
         super(GetLockerCmd, self).__init__()
-        return
 
     @staticmethod
-    def handle(config, locker, password, verbose, *args, **kwargs):
-        super(GetLockerCmd, GetLockerCmd).handle(
-            config, *args, **kwargs
-        )
-        inst = get_locker(locker, password)
-        if inst.lock_file.exists():
-            click.echo(f"confirmed lock file {inst.lock_file} exists")
+    def handle(*args, **kwargs):
+        """Get a Locker"""
+        # parent class sets up environment variables from args
+        super(GetLockerCmd, GetLockerCmd).handle(*args, **kwargs)
+        inst = get_locker_args(*args, **kwargs)
         if inst.path.exists():
             click.echo(f"confirmed locker dir {inst.path} exists")
-        print(f"{inst} verbose: {verbose} todo")
+        if inst.lock_file.exists():
+            click.echo(f"confirmed lock file {inst.lock_file} exists")
+            click.echo(f"Lock file {inst.lock_file}")
+            click.echo(f"Created {inst.timestamp}")
+            click.echo(f"Crypt ID {inst.crypt_impl.crypt_id}")
         return inst
 
 
-options = {
-    'verbose': click.option(
-        '--verbose',
-        help="Do you want a verbose report?",
-        prompt="verbose", default=False, type=bool
-    )
-}
+class GetNamedLockerCmd(GetLockerCmd):
+    """
+    Class to create and run click command to delete a named locker
+    """
 
-get_locker_cmd = GetLockerCmd.make_click_command(
-    'get-locker', options, exclude_common=False
-)
+    options = {
+        'config': config_option,
+        'locker': locker_name_option,
+        'password': password_option
+    }
+
+    def __init__(self):
+        super(GetNamedLockerCmd, self).__init__()
