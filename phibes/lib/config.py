2f97ae16d726012c8212f200f098f52bf12ab9d0
Glen Smith <github@smithsrock.com>
2021-02-24 13:54:37 -0700

    Big brittle mess in config that passes tests for now but needs a lot more work and you are thinking "why isn't this commit message in the imperative form?"

--- phibes/lib/config.py
+++ phibes/lib/config.py
@@ -13,10 +13,14 @@ from typing import Optional, Union
 # Third party packages
 # In-project modules
 from phibes.lib.errors import PhibesConfigurationError
+from phibes.lib.utils import get_debug_info, get_path_tail
 
 
 CONFIG_FILE_NAME = '.phibes.cfg'
 DEFAULT_STORE_PATH = '.phibes'
+count = 0
+none_recs = {}
+all_recs = {}
 
 
 class StoreType(enum.Enum):
@@ -36,6 +40,10 @@ class ConfigModel(object):
         Accessor for configuration store path
         :return: protected _store_path attribute
         """
+        if self._store_path is None:
+            self._store_path = Path(environ.get('PHIBES_STORE_PATH', None))
+            # raise PhibesConfigurationError(f'{self._store_path=}')
+            # raise PhibesConfigurationError('store_path not set')
         return self._store_path
 
     @store_path.setter
@@ -50,16 +58,110 @@ class ConfigModel(object):
         self._validate_store_path(new_val)
         self._store_path = new_val
         environ['PHIBES_STORE_PATH'] = f"{self._store_path}"
-        return
 
-    def __init__(self, store_path: Union[Path, str] = None):
+    def _set_private_property(
+            self, name: str, value=None, optional: bool = False
+    ):
+        if value is not None:
+            setattr(self, name, value)
+        else:
+            if optional:
+                setattr(self, f'_{name}', value)
+            else:
+                # let the property mutator complain
+                setattr(self, f'{name}', value)
+
+    def __init__(
+            self,
+            # store_path: Union[Path, str] = None,
+            **kwargs
+    ):
         """
         Create a config object
         @param store_path: overriding store_path
         """
-        self.store_path = (
-            store_path, environ.get("PHIBES_STORE_PATH", None)
-        )[store_path is None]
+        global count, none_recs, all_recs
+        debugging = False
+        if debugging:
+            count += 1
+            spa = kwargs.get('store_path', None)
+            if isinstance(spa, Path):
+                spa = spa.resolve()
+            if spa:
+                spa = get_path_tail(pth=Path(spa), number_of_nodes=3)
+            else:
+                spa = ' no kwarg `store_path` '
+            tmp_pth = environ.get('PHIBES_STORE_PATH', None)
+            if tmp_pth:
+                tmp_pth = get_path_tail(
+                    pth=Path(tmp_pth), number_of_nodes=3
+                )
+            else:
+                tmp_pth = ' no env var `PHIBES_STORE_PATH` '
+            trace, trace_depth = get_debug_info(
+                    message='ConfigModel.__init__',
+                    start_frame=1, num_frames=12, stop_val='tests/conftest.py'
+                )
+            all_recs[count] = {
+                'kwarg_keys': ','.join(list(kwargs.keys())),
+                'store_path_arg': spa, 'env_store_path': tmp_pth,
+                'trace': trace, 'trace_depth': trace_depth
+            }
+            key_list = list(kwargs.keys())
+            all_recs[count]['kwarg_keys'] = (
+                ', '.join(key_list), ' no kwargs passed '
+            )[not key_list]
+
+        # Prevent IDE from complaining elsewhere about assignment
+        self._store_path = None
+        required = []
+        optional = ['store_path']
+        err_list = []
+        for name in optional + required:
+            arg_val = kwargs.get(name, None)
+            try:
+                self._set_private_property(
+                    name=name,
+                    value=arg_val,
+                    optional=(name not in required)
+                )
+            except Exception as err:
+                err_list.append(err)
+        if err_list:
+            raise PhibesConfigurationError(f'missing arg(s): {err_list}')
+
+        limit_calls = False
+        arg_val = kwargs.get('store_path', None)
+        if arg_val is None:
+            self.store_path = environ.get("PHIBES_STORE_PATH", None)
+            if debugging:
+                none_recs[count] = all_recs[count]
+                buffer = ' ' * 5
+                msg = (
+                    f'{buffer}{count=}{buffer}'
+                    f'{buffer}{json.dumps(all_recs, indent=4)}{buffer}'
+                )
+                if count > 12 and limit_calls:
+                    raise PhibesConfigurationError(msg)
+            # pytest-of-gsmith/pytest-1074/test_normal0
+            # try:
+            #     self._set_private_property(
+            #         name='store_path', value=None, optional=True
+            #     )
+            # except Exception as err:
+            #     all_recs[count]['err'] = f"{err}"
+        else:
+            self.store_path = arg_val
+
+        # These two 'work' when store_path is a function param
+        # if store_path is None:
+        #     self.store_path = environ.get("PHIBES_STORE_PATH", None)
+        # else:
+        #     self.store_path = store_path
+        # self.store_path = (
+        #     store_path, environ.get("PHIBES_STORE_PATH", None)
+        # )[store_path is None]
+
         self.apply()
 
     def __str__(self):
@@ -135,7 +237,7 @@ def load_config_file(path: Path) -> ConfigModel:
     with conf_file.open('r') as cf:
         conf_dict = json.loads(cf.read())
     conf_mod = ConfigModel(
-        conf_dict.get(
+        store_path=conf_dict.get(
             'store_path', Path.joinpath(parent_dir, DEFAULT_STORE_PATH)
         )
     )
