815490da5b6d93a48c3f303e68cd4327476328ec
Glen Smith <github@smithsrock.com>
2021-03-24 15:48:13 -0600

    Update lib/test_config to include store

--- tests/lib/test_config.py
+++ tests/lib/test_config.py
@@ -20,15 +20,27 @@ class TestConfig(object):
         load_config_file(Path(datadir["default.json"]))
         test_config = ConfigModel()
         assert test_config.store_path == Path('.')
+        assert test_config.store == {
+            'store_path': '.',
+            'store_type': 'FileSystem'
+        }
 
     @pytest.mark.positive
     def test_hash_false(self, datadir):
         load_config_file(Path(datadir["hash_false.json"]))
         test_config = ConfigModel()
         assert test_config.store_path == Path('.')
+        assert test_config.store == {
+            'store_path': '.',
+            'store_type': 'FileSystem'
+        }
 
     @pytest.mark.positive
     def test_path_home(self, datadir):
         load_config_file(Path(datadir["path_home.json"]))
         test_config = ConfigModel()
         assert test_config.store_path == Path('~')
+        assert test_config.store == {
+            'store_path': '~',
+            'store_type': 'FileSystem'
+        }
