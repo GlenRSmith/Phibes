c9c5400dc628114756797c5a4db36f77f46bcc94
Glen Smith <github@smithsrock.com>
2021-04-27 14:02:10 -0600

    Add tests for views.get_item

---
+++ tests/views/test_get_item.py
@@ -0,0 +1,76 @@
+"""
+pytest module for view:get_locker
+"""
+
+# Standard library imports
+
+# Related third party imports
+import pytest
+
+# Local application/library specific imports
+from phibes.lib.errors import PhibesNotFoundError
+from phibes.lib.views import get_item
+from phibes.model import Locker
+
+# Local test imports
+from tests.lib.test_helpers import PopulatedLocker
+
+
+class TestGetItem(PopulatedLocker):
+    """
+    Test the get_locker view function
+    """
+
+    item_name = "get_item_via_view"
+    missing_item_name = 'do_not_get_this'
+
+    def custom_setup(self, tmp_path):
+        super(TestGetItem, self).custom_setup(tmp_path)
+        my_item = self.my_locker.create_item(self.item_name)
+        my_item.content = f"{self.item_name}"
+        self.my_locker.add_item(my_item)
+
+    def custom_teardown(self, tmp_path):
+        super(TestGetItem, self).custom_teardown(tmp_path)
+        try:
+            Locker.delete(password=self.password, locker_name=self.locker_name)
+        except PhibesNotFoundError:
+            pass
+
+    @pytest.mark.positive
+    def test_get_item(self, tmp_path, datadir, setup_and_teardown):
+        inst = get_item(
+            password=self.password,
+            locker_name=self.locker_name,
+            item_name=self.item_name
+        )
+        assert inst
+        assert type(inst) is dict
+        for fv in inst.values():
+            assert self.my_locker.crypt_impl.key not in fv
+        for locker_name, lck in self.lockers.items():
+            inst = get_item(
+                password=self.password,
+                locker_name=self.locker_name,
+                item_name=self.item_name
+            )
+            assert inst
+            assert type(inst) is dict
+            for fv in inst.values():
+                assert lck.crypt_impl.key not in fv
+
+    @pytest.mark.negative
+    def test_get_wrong_name(self, setup_and_teardown):
+        with pytest.raises(PhibesNotFoundError):
+            get_item(
+                password=self.password,
+                locker_name=self.locker_name,
+                item_name=self.missing_item_name
+            )
+        for locker_name in self.lockers.keys():
+            with pytest.raises(PhibesNotFoundError):
+                get_item(
+                    password=self.password,
+                    locker_name=locker_name,
+                    item_name=self.missing_item_name
+                )
